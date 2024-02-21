a9f828969cefacd6a038bed5ec4c41bd741c2b4e
EC2 Default User <ec2-user@ip-172-31-18-28.us-east-2.compute.internal>
2024-02-21 14:40:04 +0000

    those are all terraform to jenkins files

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "one" {
+  ami = "ami-02ca28e7c7b8f8be1"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-2a"
+  vpc_security_group_ids = [aws_security_group.terraform-sg.id]
+  user_data = <<EOF
+#!/bin/bash
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo chkconfig httpd on
+sudo echo "Hai All This is My Swiggy-1 Web-Application Created by Terraform Infrastructute By Java Home Cloud Server-1" > /var/www/html/index.html
+EOF
+
+
+  tags = {
+    Name = "swiggy-1"
+  }
+}
+
+
+resource "aws_instance" "two" {
+  ami = "ami-02ca28e7c7b8f8be1"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-2b"
+  vpc_security_group_ids = [aws_security_group.terraform-sg.id]
+  user_data = <<EOF
+#!/bin/bash
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo chkconfig httpd on
+sudo echo "Hai All This is My Swiggy-2 Web-Application Created by Terraform Infrastructute By Java Home Cloud Server-1" > /var/www/html/index.html
+EOF
+
+
+  tags = {
+    Name = "swiggy-2"
+  }
+}
